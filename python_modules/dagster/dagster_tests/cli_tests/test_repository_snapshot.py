771b9dda827ce18d73ef670358b373fc407c8292
asingh16 <abhi@elementl.com>
2020-03-28 20:18:12 -0700

    Add repository snapshot CLI
    Summary: This revision adds a repository snapshot CLI as well as a RepositorySnapshot while also taking steps to tighten the serdes rules around PipelineSnapshots so that we can serialize/deserialize these snapshots between containers. This revision also adds tests.
    
    Test Plan: unit
    
    Differential Revision: https://dagster.phacility.com/D2358

---
+++ python_modules/dagster/dagster_tests/cli_tests/test_repository_snapshot.py
@@ -0,0 +1,37 @@
+from click.testing import CliRunner
+
+from dagster import file_relative_path
+from dagster.cli.repository import snapshot_command
+from dagster.core.serdes import deserialize_json_to_dagster_namedtuple
+from dagster.core.snap.pipeline_snapshot import PipelineSnapshot
+from dagster.core.snap.repository_snapshot import RepositorySnapshot
+from dagster.utils import safe_tempfile_path
+
+
+def test_snapshot_command_handle_repository():
+    runner = CliRunner()
+    with safe_tempfile_path() as fp:
+        result = runner.invoke(
+            snapshot_command, [fp, '-y', file_relative_path(__file__, 'repository_file.yaml')],
+        )
+        assert result.exit_code == 0
+        # Now that we have the snapshot make sure that it can be properly deserialized
+        with open(fp) as buffer:
+            repository_snap = deserialize_json_to_dagster_namedtuple(buffer.read())
+        assert isinstance(repository_snap, RepositorySnapshot)
+        assert repository_snap.name == 'bar'
+        assert len(repository_snap.pipeline_snapshots) == 2
+
+
+def test_snapshot_command_handle_pipeline():
+    runner = CliRunner()
+    with safe_tempfile_path() as fp:
+        result = runner.invoke(
+            snapshot_command,
+            [fp, '-f', file_relative_path(__file__, 'test_cli_commands.py'), '-n', 'baz_pipeline'],
+        )
+        assert result.exit_code == 0
+        with open(fp) as buffer:
+            pipeline_snap = deserialize_json_to_dagster_namedtuple(buffer.read())
+        assert isinstance(pipeline_snap, PipelineSnapshot)
+        assert pipeline_snap.name == 'baz'
