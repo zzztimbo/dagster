e809902df351816bcc5c7da9177693d72560626b
asingh16 <abhi@elementl.com>
2020-03-30 15:04:25 -0700

    got rid of snapshots since they weren't very stable across versions and because pipeline snapshot tests are doing ti manually

--- python_modules/dagster/dagster_tests/core_tests/snap_tests/test_repository_snap.py
+++ python_modules/dagster/dagster_tests/core_tests/snap_tests/test_repository_snap.py
@@ -1,9 +1,9 @@
 from dagster import RepositoryDefinition, pipeline, solid
+from dagster.core.snap.pipeline_snapshot import PipelineSnapshot
 from dagster.core.snap.repository_snapshot import RepositorySnapshot
-from dagster.serdes import serialize_dagster_namedtuple
 
 
-def test_repository_snap_all_props(snapshot):
+def test_repository_snap_all_props():
     @solid
     def noop_solid(_):
         pass
@@ -15,10 +15,16 @@ def test_repository_snap_all_props(snapshot):
     repo = RepositoryDefinition(name='noop_repo', pipeline_defs=[noop_pipeline])
     repo_snap = RepositorySnapshot.from_repository_definition(repo)
 
-    snapshot.assert_match(serialize_dagster_namedtuple(repo_snap))
+    assert repo_snap.name == 'noop_repo'
+    assert len(repo_snap.pipeline_snapshots) == 1
+    assert isinstance(repo_snap.pipeline_snapshots[0], PipelineSnapshot)
+    assert repo_snap.pipeline_snapshots[0].name == 'noop_pipeline'
+    assert repo_snap.pipeline_snapshots[0].description is None
+    assert repo_snap.pipeline_snapshots[0].tags == {}
 
 
-def test_repository_snap_empty(snapshot):
+def test_repository_snap_empty():
     repo = RepositoryDefinition(name='empty_repo', pipeline_defs=[])
     repo_snap = RepositorySnapshot.from_repository_definition(repo)
-    snapshot.assert_match(serialize_dagster_namedtuple(repo_snap))
+    assert repo_snap.name == 'empty_repo'
+    assert len(repo_snap.pipeline_snapshots) == 0
