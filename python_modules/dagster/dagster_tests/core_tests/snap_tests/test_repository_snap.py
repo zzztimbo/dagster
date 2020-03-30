3692cbcdae7930b62ce84a511e01e64edeb529c4
asingh16 <abhi@elementl.com>
2020-03-30 14:33:15 -0700

    fixed snapshot bug

--- python_modules/dagster/dagster_tests/core_tests/snap_tests/test_repository_snap.py
+++ python_modules/dagster/dagster_tests/core_tests/snap_tests/test_repository_snap.py
@@ -1,5 +1,6 @@
 from dagster import RepositoryDefinition, pipeline, solid
 from dagster.core.snap.repository_snapshot import RepositorySnapshot
+from dagster.serdes import serialize_dagster_namedtuple
 
 
 def test_repository_snap_all_props(snapshot):
@@ -14,10 +15,10 @@ def test_repository_snap_all_props(snapshot):
     repo = RepositoryDefinition(name='noop_repo', pipeline_defs=[noop_pipeline])
     repo_snap = RepositorySnapshot.from_repository_definition(repo)
 
-    snapshot.assert_match(repo_snap)
+    snapshot.assert_match(serialize_dagster_namedtuple(repo_snap))
 
 
 def test_repository_snap_empty(snapshot):
     repo = RepositoryDefinition(name='empty_repo', pipeline_defs=[])
     repo_snap = RepositorySnapshot.from_repository_definition(repo)
-    snapshot.assert_match(repo_snap)
+    snapshot.assert_match(serialize_dagster_namedtuple(repo_snap))
