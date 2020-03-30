5a65f3a51557c66fc1ab4d5d359a76019ac07144
Ben Gotow <ben@foundry376.com>
2020-03-30 11:40:52 -0500

    Replace the playground's execution plan preview with a more useful config map
    Summary: The usefulness of the playground's run preview has been pretty limited so far and this diff replaces it with a new "summary" panel that shows you which parts of your config are valid, which are missing, and which contain errors. You can click errors to jump to them, and click sections to jump to them (though the top panel always shows the entire document - sub-"views" might be an interesting stacked diff). The "Solids" section of the summary is not currently a representation of the running order or anything like that - I think it'd be interesting, but I'm not totally convinced that the whitespace required to render a plan / gaant diagram, etc. is worth it in this view.
    
    I also made the errors a more straightforward list so we don't run into issues with them being clipped off the screen.
    
    Test Plan: Updated snapshots
    
    Reviewers: prha, alangenfeld, schrockn
    
    Subscribers:

--- js_modules/dagit/src/configeditor/ConfigEditor.tsx
+++ js_modules/dagit/src/configeditor/ConfigEditor.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { createGlobalStyle } from "styled-components/macro";
+import * as yaml from "yaml";
 import "codemirror";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
@@ -24,8 +25,10 @@ import {
 } from "./types/ConfigEditorEnvironmentSchemaFragment";
 import "./codemirror-yaml/mode";
 import {
-  LintJson as YamlModeLintJson,
-  expandAutocompletionContextAtCursor
+  ValidationError as YamlModeValidationError,
+  ValidateFunction as YamlModeValidateFunction,
+  expandAutocompletionContextAtCursor,
+  validationErrorToCodemirrorError
 } from "./codemirror-yaml/mode";
 import { debounce } from "../Util";
 
@@ -37,13 +40,14 @@ export function isHelpContextEqual(
 }
 
 interface ConfigEditorProps {
-  checkConfig: YamlModeLintJson;
   configCode: string;
-  onConfigChange: (newValue: string) => void;
-  onHelpContextChange: (helpContext: ConfigEditorHelpContext | null) => void;
-  environmentSchema?: ConfigEditorEnvironmentSchemaFragment;
   readOnly: boolean;
   showWhitespace: boolean;
+  environmentSchema?: ConfigEditorEnvironmentSchemaFragment;
+
+  checkConfig: YamlModeValidateFunction;
+  onConfigChange: (newValue: string) => void;
+  onHelpContextChange: (helpContext: ConfigEditorHelpContext | null) => void;
 }
 
 const AUTO_COMPLETE_AFTER_KEY = /^[a-zA-Z0-9_@(]$/;
@@ -93,6 +97,41 @@ export class ConfigEditor extends React.Component<ConfigEditorProps> {
     if (prevProps.environmentSchema === this.props.environmentSchema) return;
     this.performInitialPass();
   }
+
+  shouldComponentUpdate(prevProps: ConfigEditorProps) {
+    // Unfortunately, updates to the ConfigEditor clear the linter highlighting for
+    // unknown reasons and they're recalculated asynchronously. To prevent flickering,
+    // only update if our input has meaningfully changed.
+    return (
+      prevProps.configCode !== this.props.configCode ||
+      prevProps.readOnly !== this.props.readOnly ||
+      prevProps.showWhitespace !== this.props.showWhitespace
+    );
+  }
+
+  // Public API
+
+  moveCursor = (line: number, ch: number) => {
+    this._editor?.scrollIntoView({ line, ch }, 200);
+  };
+
+  moveCursorToValidationError = (err: YamlModeValidationError) => {
+    if (!this._editor) return;
+    const codeMirrorDoc = this._editor.getDoc();
+    const yamlDoc = yaml.parseDocument(this.props.configCode);
+    const lintError = validationErrorToCodemirrorError(
+      err,
+      yamlDoc,
+      codeMirrorDoc
+    );
+    if (lintError) {
+      this._editor?.setSelection(lintError.from, lintError.to, {
+        scroll: true
+      });
+    }
+  };
+
+  // End Public API
 
   performInitialPass() {
     // update the gutter and redlining
